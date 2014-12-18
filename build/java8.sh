fb3c0d00e85229afaa5eb710f8583799bd8b508b
John Eckhart <jeckhart@yodle.com>
2014-12-18 12:15:16 -0500

    Switch to the debian based yodle-base image

--- build/java8.sh
+++ build/java8.sh
@@ -5,9 +5,9 @@ source /build/docker-yodle-base/profile
 $apt_get update
 
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-add-apt-repository ppa:webupd8team/java -y
-apt-get update -y
-apt-get install oracle-java8-installer -y
+add-apt-repository ppa:webupd8team/java trusty
+$apt_get update
+$apt_get install oracle-java8-installer
 
 # Don't leave this dir around, it just consumes space
 rm -rf /var/cache/oracle-jdk8-installer
